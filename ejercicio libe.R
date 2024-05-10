4acdde10ff7fbfefcee5bd597ce19075624ff4e9
Anne Martin <anne.martinb@alumni.mondragon.edu>
2024-05-10 12:17:24 +0200

    tercer commit

--- ejercicio libe.R
+++ ejercicio libe.R
@@ -1,6 +1,7 @@
 setwd("C://Users//Anne//Desktop//BDATA1//ejercicio libe//ejercicio-libe//datos")
 library(readxl)
 library(lubridate)
+library(dplyr)
 datos <- read_xlsx("online_retail_II.xlsx")
 
 datos$InvoiceDate <- ymd_hms(datos$InvoiceDate)
@@ -14,3 +15,6 @@ length(datos)
 dim(datos)
 
 perdidos_diferentes <- length(unique(datos$Invoice))
+
+precio_medio <- datos %>% group_by(Invoice) %>% summarise(media = mean(n()))
+media_producto <- datos %>% group_by(Invoice) %>% summarise(total = sum(Quantity))
